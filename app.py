0020ecca19dc1eb4afc13d425a6491b91f589ad7
donniejr107 <julyanorisdianto@gmail.com>
2025-06-01 13:38:04 +0700

    test

--- app.py
+++ app.py
@@ -33,7 +33,3 @@ def predict():
         return jsonify({'class': result, 'confidence': float(np.max(preds[0]))})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
